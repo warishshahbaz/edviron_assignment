e7cf23e038451db4e0c318d4fa70c9bb740806a6
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-12-26 23:26:35 +0530

    update table

--- src/components/tableCom.jsx
+++ src/components/tableCom.jsx
@@ -12,6 +12,20 @@ const TableComp = ({
   currentPage,
   isDarkMode, // Dark mode state
 }) => {
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-[300px]">
+        <CircularProgress color="inherit" />
+      </div>
+    );
+  }
+
+  if (currentRows?.length === 0) {
+    return (
+      <div className="flex justify-center items-center h-[300px]">No Data</div>
+    );
+  }
+
   return (
     <div
       className={`overflow-x-auto rounded-lg shadow ${
@@ -30,80 +44,58 @@ const TableComp = ({
             <th className="px-4 py-2 border">School Name</th>
             <th className="px-4 py-2 border">Collect ID</th>
             <th className="px-4 py-2 border">Status</th>
-            <th className="px-4 py-2 border">Transaction Amount</th>
+            <th className="px-4 py-2 border">Custom Order</th>
             <th className="px-4 py-2 border">Payment Method</th>
             <th className="px-4 py-2 border">Date</th>
           </tr>
         </thead>
         <tbody>
-          {loading ? (
-            <tr>
-              <td
-                colSpan="6"
-                className="text-center flex justify-center items-center h-[300px]"
-              >
-                <CircularProgress color="inherit" />
-              </td>
-            </tr>
-          ) : currentRows?.length === 0 ? (
-            <tr>
-              <td
-                colSpan="6"
-                className="text-center flex justify-center items-center h-[300px]"
-              >
-                No Data
-              </td>
-            </tr>
-          ) : (
-            currentRows?.map((row, index) => (
-              <tr
-                key={index}
-                className={`${
-                  index % 2 === 0
-                    ? isDarkMode
-                      ? "bg-gray-700"
-                      : "bg-gray-50"
-                    : "bg-white"
-                } ${
-                  index % 1 === 0
-                    ? isDarkMode
-                      ? "hover:bg-gray-700"
-                      : "hover:bg-gray-50"
-                    : "hover:bg-white"
-                } hover:scale-105 hover:shadow-md transition-transform duration-300 ease-in-out`}
-              >
-                <td className="px-4 py-2 border capitalize">
-                  {row.school_name}
-                </td>
-                <td className="px-4 py-2 border flex items-center">
-                  {row.collect_id}
-                  <IconButton
-                    size="small"
-                    onClick={() => handleCopy(row.collect_id)}
-                    className="ml-2"
-                  >
-                    <MdContentCopy />
-                  </IconButton>
-                </td>
-                <td
-                  className={`px-4 py-2 border ${
-                    row.status === "SUCCESS"
-                      ? "text-green-500"
-                      : row.status === "PENDING"
-                      ? "text-yellow-500"
-                      : "text-red-500"
-                  }`}
+          {currentRows?.map((row, index) => (
+            <tr
+              key={index}
+              className={`${
+                index % 2 === 0
+                  ? isDarkMode
+                    ? "bg-gray-700"
+                    : "bg-gray-50"
+                  : "bg-white"
+              } ${
+                index % 1 === 0
+                  ? isDarkMode
+                    ? "hover:bg-gray-700"
+                    : "hover:bg-gray-50"
+                  : "hover:bg-white"
+              } hover:scale-105 hover:shadow-md transition-transform duration-300 ease-in-out`}
+            >
+              <td className="px-4 py-2 border capitalize">{row.school_name}</td>
+              <td className="px-4 py-2 border flex items-center">
+                {row.collect_id}
+                <IconButton
+                  size="small"
+                  onClick={() => handleCopy(row.collect_id)}
+                  className="ml-2"
                 >
-                  {row.status}
-                </td>
-                <td className="px-4 py-2 border">{row.transaction_amount}</td>
-                <td className="px-4 py-2 border">{row.gateway}</td>
-                <td className="px-4 py-2 border">
-                  {row.created_at ? row.created_at.split("T")[0] : "N/A"}
-                </td>
-              </tr>
-            ))
-          )}
+                  <MdContentCopy />
+                </IconButton>
+              </td>
+              <td
+                className={`px-4 py-2 border ${
+                  row.status === "SUCCESS"
+                    ? "text-green-500"
+                    : row.status === "PENDING"
+                    ? "text-yellow-500"
+                    : "text-red-500"
+                }`}
+              >
+                {row.status}
+              </td>
+              <td className="px-4 py-2 border">{row.custom_order_id}</td>
+              <td className="px-4 py-2 border">{row.gateway}</td>
+              <td className="px-4 py-2 border">
+                {row.created_at ? row.created_at.split("T")[0] : "N/A"}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
 
